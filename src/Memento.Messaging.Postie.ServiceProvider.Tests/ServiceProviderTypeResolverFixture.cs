c0a8cf08716197895dea8ffbb0648752d9563caf
Andrea Saltarello <andysal@gmail.com>
2017-03-10 18:09:49 +0100

    First commit

---
+++ src/Memento.Messaging.Postie.ServiceProvider.Tests/ServiceProviderTypeResolverFixture.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using NUnit.Framework;
+using SharpTestsEx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento.Messaging.Postie.ServiceProvider.Tests
+{
+    [TestFixture]
+    public class ServiceProviderTypeResolverFixture
+    {
+        [Test]
+        public void Ctor_should_throw_ArgumentNullException_on_null_services_parameter()
+        {
+            Executing.This(() => new ServiceProviderTypeResolver(null))
+                .Should()
+                .Throw<ArgumentNullException>()
+                .And
+                .ValueOf
+                .ParamName
+                .Should()
+                .Be
+                .EqualTo("services");
+        }
+
+        [Test]
+        public void Ctor_should_set_Container_property()
+        {
+            var serviceCollectionMock = new Mock<IServiceCollection>().Object;
+            var sut = new ServiceProviderTypeResolver(serviceCollectionMock);
+            Assert.AreSame(serviceCollectionMock, sut.Services);
+        }
+    }
+}
---
+++ src/Memento.Messaging.Postie.ServiceProvider/ServiceProviderTypeResolver.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Memento.Messaging.Postie.ServiceProvider
+{
+    public class ServiceProviderTypeResolver : ITypeResolver
+    {
+        public IServiceCollection Services { get; private set; }
+
+        public ServiceProviderTypeResolver(IServiceCollection services)
+        {
+            if (services == null)
+                throw new ArgumentNullException(nameof(services));
+            Services = services;
+        }
+
+        public object Resolve(Type t)
+        {
+            var container = Services.BuildServiceProvider();
+            return container.GetService(t);
+        }
+    }
+}
